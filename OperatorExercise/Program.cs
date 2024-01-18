9339ae67736c53cb02e2e2faeaff4a05f51a4ad1
navpreet <navpreetbatth2.nb@gmail.com>
2024-01-18 09:09:22 -0800

    intial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 10;
+            int b = 20;
+
+            int sum = a + b;
+            int subtract = a - b;
+            int product = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+
+            Console.WriteLine($"{a}+{b} is {sum}");
+            Console.WriteLine($"{a}-{b} is {subtract}");
+            Console.WriteLine($"{a}*{b} is {product}");
+            Console.WriteLine($"{a}/{b} is {quotient}");
+            Console.WriteLine($"{a}%{b} is remainder {remainder}");
+
+            Console.WriteLine("Please enter the radius");
+            double userInput = int.Parse(Console.ReadLine());
+            Console.WriteLine(AreaOfCircle(userInput));
+
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+
+        }
+
     }
 }
